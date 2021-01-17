52eb595c0b6bca085c543512bdeb05747accfd98
ersinbulut <ersinblt61@gmail.com>
2021-01-17 23:28:32 +0300

    ReactStrap İle Çalışmak

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React from 'react';
 import CategoryList from './CategoryList';
 import Navi from './Navi';
 import ProductList from './ProductList';
+import { Container, Row, Col } from 'reactstrap'
 
 function App() {
   return (
     <div>
-      <Navi></Navi>
-      <CategoryList/>
-      <ProductList/>
-   
+      <Container>
+        <Row>
+          <Navi></Navi>
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList />
+          </Col>
+          <Col xs="9">
+            <ProductList />
+          </Col>
+
+
+        </Row>
+      </Container>
+
+
+
+
     </div>
 
   );
